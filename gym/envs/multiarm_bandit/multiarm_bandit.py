a0d8c98edea9ed4f114ea0b823c0223dde08a027
Ashish <ashish.dilip.bora@gmail.com>
2016-08-28 15:31:13 -0500

    Added reset function

--- gym/envs/multiarm_bandit/multiarm_bandit.py
+++ gym/envs/multiarm_bandit/multiarm_bandit.py
@@ -3,16 +3,27 @@ import gym
 from gym import spaces
 from gym.utils import seeding
 
+
 class MultiArmedBandit(gym.Env):
 
-    def __init__(self, num_arms=10, val_mean=0, val_var=1, reward_var=1):
-    	self.action_space = spaces.Discrete(num_arms)
-    	self._seed()
+    def __init__(self, num_arms=10, value_mean=0, value_var=1, reward_var=1):
+        self.value_mean = value_mean
+        self.value_var = value_var
+        self.reward_var = reward_var
+        self.action_space = spaces.Discrete(num_arms)
+        self._seed()
         self._reset()
 
     def init_q_star(self):
-    	return np.random.normal(loc=val_mean, scale=val_var, size=[num_arms])
+        # TODO(abora) : figure out how to use random seed
+        q_star = {}
+        for action in self.action_space:
+            q_star[action] = np.random.normal(loc=self.value_mean, scale=self.value_var)
+        return q_star
 
     def _seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
+
+    def _reset(self):
+        self.q_star = self.init_q_star()
